19cd13ea711bad315d2a936cc85d15aa37cb7689
Akshay Kumar <akshaysharmav7@gmail.com>
2025-12-13 13:29:22 +0530

    testi

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>This is Liebes</h1>
+      <h1>Ich Spreche Deutsch</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
