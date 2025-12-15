31a55fac0287458fdc3f4063432decb744098a7a
Akshay Kumar <akshaysharmav7@gmail.com>
2025-12-15 18:34:11 +0530

    docker deploy test

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Ich Spreche Deutsch</h1>
+      <h1>Ich Spreche</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
